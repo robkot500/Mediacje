53165fee3d7a5d547615d6307ec846547f49312b
robkot500 <robert.kotwicki5@gmail.com>
2020-05-07 16:54:02 +0200

    baner

--- js/Header.js
+++ js/Header.js
@@ -1,22 +1,40 @@
 import React from 'react';
 import ReactDOM from "react-dom";
-import {Link as RouterLink} from "react-router-dom";
-import {Link} from 'react-scroll';
+import { Link as RouterLink } from "react-router-dom";
+import { Link } from 'react-scroll';
+
+
+
 
 const Header = () => {
     return (
         <>
-            <div className="header-container">
-                <div className="nav">
-                    <ul>
-                        <li className='start'><RouterLink to="/">Start</RouterLink></li>
-                        <li className='przebieg'><RouterLink to="/przebieg">Przebieg</RouterLink></li>
-                        <li className='zasady'><RouterLink to="/zasady">Zasady</RouterLink></li>
-                        <li className='oMnie'><RouterLink to="/oMnie">O mnie</RouterLink></li>
-                        <li className='kontakt'><RouterLink to="/kontakt">Kontakt</RouterLink></li>
-                    </ul>
+            <div className="row header-container">
+                <div className="col-12">
+                    <div className="box box-1 nav">
+                        <ul>
+                            <li className='start'><RouterLink to="/">Masz prawo do mediacji</RouterLink></li>
+                            <li className='przebieg'><RouterLink to="/przebieg">Przebieg</RouterLink></li>
+                            <li className='zasady'><RouterLink to="/zasady">Zasady</RouterLink></li>
+                            <li className='oMnie'><RouterLink to="/oMnie">O mnie</RouterLink></li>
+                            <li className='kontakt'><RouterLink to="/kontakt">Kontakt</RouterLink></li>
+                        </ul>
+                        <div className="ham-wrapper">
+                            <div className="ham1"></div>
+                            <div className="ham2"></div>
+                            <div className="ham3"></div>
+                            <ul className='mobile-wrapper'>
+                            <li className='przebieg mobile'><RouterLink to="/przebieg">Przebieg</RouterLink></li>
+                            <li className='zasady mobile'><RouterLink to="/zasady">Zasady</RouterLink></li>
+                            <li className='oMnie mobile'><RouterLink to="/oMnie">O mnie</RouterLink></li>
+                            <li className='kontakt mobile'><RouterLink to="/kontakt">Kontakt</RouterLink></li>
+                        </ul>
+                        </div>
+                    </div>
+                    <div className="box box-1 header-image">
+
+                    </div>
                 </div>
-                <div className='header-image'></div>
             </div>
         </>
     )
