fe585c53dcabb9793fabf043713b34ce7290f2b4
robkot500 <robert.kotwicki5@gmail.com>
2020-05-08 14:50:56 +0200

    next

--- js/Header.js
+++ js/Header.js
@@ -1,4 +1,4 @@
-import React,{useEffect} from 'react';
+import React, { useEffect } from 'react';
 import ReactDOM from "react-dom";
 import { Link as RouterLink } from "react-router-dom";
 import { Link } from 'react-scroll';
@@ -13,12 +13,12 @@ import { Link } from 'react-scroll';
 const Header = () => {
     useEffect(() => {
         const cont = document.querySelector(".ham-wrapper");
-    // const mobile = document.querySelector('.mobile');
-    console.log(cont);
-    cont.addEventListener('click',function () {
-        cont.classList.toggle('click')
-        //mobile.classList.toggle('mobile-switch')
-    })
+        // const mobile = document.querySelector('.mobile');
+        console.log(cont);
+        cont.addEventListener('click', function () {
+            cont.classList.toggle('click')
+            //mobile.classList.toggle('mobile-switch')
+        })
     })
     return (
         <>
@@ -26,28 +26,29 @@ const Header = () => {
                 <div className="col-12">
                     <div className="box box-1 nav">
                         <ul>
-                            <li className='start'><RouterLink to="/">Masz prawo do mediacji</RouterLink></li>
+                         <li className='start'><RouterLink to="/">Masz prawo do mediacji</RouterLink></li>
                             <li className='przebieg'><RouterLink to="/przebieg">Przebieg</RouterLink></li>
                             <li className='zasady'><RouterLink to="/zasady">Zasady</RouterLink></li>
                             <li className='oMnie'><RouterLink to="/oMnie">O mnie</RouterLink></li>
                             <li className='kontakt'><RouterLink to="/kontakt">Kontakt</RouterLink></li>
                         </ul>
-                        
+
                         <div className="ham-wrapper">
                             <div className="ham1"></div>
                             <div className="ham2"></div>
                             <div className="ham3"></div>
                             <ul className='mobile-wrapper'>
-                            <li className='przebieg mobile'><RouterLink to="/przebieg">Przebieg</RouterLink></li>
-                            <li className='zasady mobile'><RouterLink to="/zasady">Zasady</RouterLink></li>
-                            <li className='oMnie mobile'><RouterLink to="/oMnie">O mnie</RouterLink></li>
-                            <li className='kontakt mobile'><RouterLink to="/kontakt">Kontakt</RouterLink></li>
-                        </ul>
+                                <li className='przebieg mobile'><RouterLink to="/przebieg">Przebieg</RouterLink></li>
+                                <li className='zasady mobile'><RouterLink to="/zasady">Zasady</RouterLink></li>
+                                <li className='oMnie mobile'><RouterLink to="/oMnie">O mnie</RouterLink></li>
+                                <li className='kontakt mobile'><RouterLink to="/kontakt">Kontakt</RouterLink></li>
+                            </ul>
                         </div>
                     </div>
                     <div className="box box-1 header-image">
 
                     </div>
+                    <div className="green"></div>
                 </div>
             </div>
         </>
--- js/Omnie.js
+++ js/Omnie.js
@@ -3,9 +3,15 @@ import React from 'react';
 const Omnie = () =>{
     return(
         <>
+        <div class="row omnie">
+        <div className="col-12">
+                    <div className="box box-2">
         <h1>OOOOOO</h1>
             <div className="green"></div>
             <div className="yellow"></div>
+            </div>
+            </div>
+            </div>
             </>
     )
 }
