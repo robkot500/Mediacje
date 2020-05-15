312a790949f72aaf7ea4a9010be1121924039255
robkot500 <robert.kotwicki5@gmail.com>
2020-05-15 19:01:36 +0200

    next

--- js/App.js
+++ js/App.js
@@ -4,8 +4,8 @@ import './../scss/main.scss';
 import Header from "./Header"
 import { HashRouter, Switch, Route, Redirect } from "react-router-dom";
 import Home from "./Home";
-import Przebieg from "./Przebieg";
-import Zasady from "./Zasady";
+import Process from "./Process";
+import Rules from "./Rules";
 import About from "./About";
 import Contact from "./Contact";
 import ScrollToTop from "./ScrollToTop"
@@ -21,8 +21,8 @@ const App = () => {
                     <Switch>
 
                         <Route exact path="/" component={Home} />
-                        <Route path="/przebieg" component={Przebieg} />
-                        <Route path="/zasady" component={Zasady} />
+                        <Route path="/przebieg" component={Process} />
+                        <Route path="/zasady" component={Rules} />
                         <Route path="/oMnie" component={About} />
                         <Route path="/kontakt" component={Contact} />
 
